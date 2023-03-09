8d1e1c32e6f3ed3b1f89aad3d37457048ea6bb9d
DouglasDainese <douglas_dainese@hotmail.com>
2023-03-09 18:54:31 -0300

    Resolução exercicio 1 - função 2

--- Dia-1/my-ts-scripts/exercicio1.ts
+++ Dia-1/my-ts-scripts/exercicio1.ts
@@ -35,3 +35,26 @@ const isItAPrime = (param: number): boolean => {
 isItAPrime(numberInput)
   ? console.log(`${numberInput} é primo`)
   : console.log(`${numberInput} não é primo`);
+
+/* Essa função é responsável por validar se o formato do e-mail está correto.
+
+const emailInput = 'email@email.com';
+
+const validateEmailFormat = (param) => {
+var re = /\S+@\S+\.\S+/;
+return re.test(param);
+};
+
+console.log(validateEmailFormat(emailInput))
+*/
+
+// Essa função é responsável por validar se o formato do e-mail está correto.
+
+const emailInput: string = 'email@email.com';
+
+const validateEmailFormat = (param: string): boolean => {
+  const re: RegExp = /\S+@\S+\.\S+/;
+  return re.test(param);
+};
+
+console.log(validateEmailFormat(emailInput))
